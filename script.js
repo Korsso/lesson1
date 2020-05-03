4e41fad6e04f386997f85fdd40607b17934a4cea
Valeri <korsso74@gmail.com>
2020-05-03 14:01:52 +0300

    урок 1

---
+++ script.js
@@ -0,0 +1,19 @@
+//alert('Hello!');
+//alert('Hello!');  
+     //console.log(3 + '2');
+     //let a = 5;
+     //a =8 + a;
+     //console.log(a);
+     // alert(a);
+     //out.innerHTML = '<i>Hi</i>';
+     //out.innerText = '<i>Hi</i>'; 
+     //document.getElementById('out').innerHTML =777;
+     let p;
+     p = document.getElementById('out');
+     p =document.querySelector('#out');
+     p.innerHTML = 558885;
+     console.log(p);
+     let c = 'hello';
+     let d = 'wordl';
+     console.log(c +d);
+     
