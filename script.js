8a441f1b4708ca34f541aed3d7521f036317ca06
Korsso <64301604+Korsso@users.noreply.github.com>
2021-03-06 18:32:01 +0200

    commit-2

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
-//alert('Hello!');
-//alert('Hello!');  
-     //console.log(3 + '2');
-     //let a = 5;
-     //a =8 + a;
-     //console.log(a);
-     // alert(a);
-     //out.innerHTML = '<i>Hi</i>';
-     //out.innerText = '<i>Hi</i>'; 
-     //document.getElementById('out').innerHTML =777;
-     //let p;
-     //p = document.getElementById('out');
-     //p =document.querySelector('#out');
-     //p.innerHTML = 558885;
-    // console.log(p);
-     //let c = 'hello';
-     //let d = 'wordl';
-     //console.log(c +d);
-    // let year = 1974;
-     //console.log(2020 - 1974);
-     //let p;
-     //p =document.querySelector('#out');
-     //p.innerHTML = 1974;
-     if (2*4 == 8) {
-          console.log("Верно")
-     }
+
+var leftBorderWidth = 1;
+
+
+    
+
+let second = 2; 
+
+
+
+const pi = 3.14;
+
+console.log(second);
