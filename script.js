abe0e36cf9bc0571bf7d73915be88a27245cf4e0
Korsso <64301604+Korsso@users.noreply.github.com>
2021-03-07 11:53:10 +0200

    commit-3

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 
-var leftBorderWidth = 1;
+//var leftBorderWidth = 1;
+//let second = 2; 
+  //const pi = 3.14;
+ // console.log(second);  
 
 
-    
+var number = 5;
+var string = "Hello!";
+var syn = Symbol();
+null;
+undefined;
+var obj = ();
 
-let second = 2; 
+console.log(4/0);
 
 
 
-const pi = 3.14;
 
-console.log(second);
+
